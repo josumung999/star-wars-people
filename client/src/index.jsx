454997a5e5eacde11e3089eff6b26681852a675b
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-08 10:15:46 +0200

    Apollo Client basic setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Setting up apollo client
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
