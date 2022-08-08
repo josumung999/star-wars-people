1651e22aeeed6aba8a81d0d263947e81b7e6ce67
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-08 10:36:41 +0200

    Fetching data from GraphQL API

--- client/src/pages/People.jsx
+++ client/src/pages/People.jsx
@@ -1,12 +1,35 @@
 import { Grid, Container, Pagination } from "@nextui-org/react";
-import { people } from "../constants/data";
+import { gql, useQuery } from "@apollo/client";
 import PeopleItem from '../components/PeopleItem';
 
-export default function Product() {
+
+const PEOPLE = gql`
+  query AllPeople {
+    allPeople {
+      id
+      name
+      height
+      mass
+      hair_color
+      skin_color
+      eye_color
+      birth_year
+      gender
+      homeworld
+    }
+  }
+`;
+
+export default function People() {
+  const { loading, error, data } = useQuery(PEOPLE);
+
+  if (loading) return 'Loading ...';
+
+  if (error) return `Error! ${error.message}`;
   return (
     <Container>
       <Grid.Container gap={2} justify="center">
-        {people.map((person) => (
+        {data?.allPeople?.map((person) => (
           <PeopleItem key={person.id} person={person} />
         ))}
       </Grid.Container>
